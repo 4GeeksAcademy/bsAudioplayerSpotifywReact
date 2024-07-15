34e3d26caab4d5f4a3bca26ead1c69e26339ddd0
Billy Storm <billstormbiz@gmail.com>
2024-07-15 20:38:10 +0000

    finished

--- src/js/component/song.jsx
+++ src/js/component/song.jsx
@@ -1,29 +1,84 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Song = () => {
   const [songs, setSongs] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentlyPlaying, setCurrentlyPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  const baseURL = "https://playground.4geeks.com";
+  var audioRef = useRef();
+
   useEffect(() => {
-    fetch("https://playground.4geeks.com/sound/songs")
+    fetch(baseURL + "/sound/songs")
       .then((response) => response.json())
       .then((data) => {
         setSongs(data.songs);
-      })
-      .then(console.log(songs));
+      });
   }, []);
 
-    function playSong () {
-        
+  useEffect(() => {
+    audioRef.play();
+    if (isPlaying) {
+      audioRef.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.pause();
+      setIsPlaying(false);
     }
+    console.log("here: " + audioRef.src);
+  }, [currentlyPlaying, isPlaying]);
+
+  function playSong(song) {
+    setIsPlaying(true);
+    setCurrentlyPlaying(baseURL + song.url);
+    setCurrentSong(song);
+  }
+
+  function handleLeft() {
+    if (currentSong.id == songs[0].id) {
+      setCurrentSong(songs[songs.length - 1]);
+      const index = songs.length - 1;
+      audioRef.src = baseURL + songs[index].url;
+      setCurrentlyPlaying(baseURL + songs[index].url);
+      setIsPlaying(true);
+      audioRef.play();
+    } else {
+      const index = currentSong.id - 1;
+      audioRef.src = baseURL + songs[index - 1].url;
+      setCurrentlyPlaying(baseURL + songs[index - 1].url);
+      setCurrentSong(songs[index - 1]);
+      setIsPlaying(true);
+      audioRef.play();
+    }
+  }
+
+  function handleRight() {
+    if (currentSong.id == songs.length) {
+      setCurrentSong(songs[0]);
+    } else {
+      const index = currentSong.id + 1;
+      audioRef.src = baseURL + songs[index + 1].url;
+      setCurrentlyPlaying(baseURL + songs[index + 1].url);
+      setCurrentSong(songs[index + 1]);
+      setIsPlaying(true);
+      audioRef.play();
+    }
+  }
   return (
     <>
       <div className="container-fluid">
-        <h1 className="header">Not Spotify...</h1>
+        <audio ref={(e) => (audioRef = e)} src={currentlyPlaying}></audio>
+        <h1 className="header px-5">Not Spotify...</h1>
         <div className="container-fluid p-0">
           <div id="songs" className="row p-0">
             {songs.map((song, index) => {
               return (
-                <p key={index} className="songText songRow">
+                <p
+                  onClick={() => playSong(song)}
+                  key={index}
+                  className="songText songRow"
+                >
+                  {" "}
                   {song.id}: {song.name}
                 </p>
               );
@@ -31,17 +86,35 @@ const Song = () => {
           </div>
         </div>
         <div className="footer fixed-bottom text-center">
-          <button className="skipButtons border rounded border-0">
+          <button
+            className="skipButtons border rounded border-0"
+            onClick={() => handleLeft()}
+          >
             <i className="fa solid fa-caret-left"></i>
           </button>
           {isPlaying ? (
-            <button>pause</button>
+            <button
+              onClick={() => {
+                setIsPlaying(false);
+              }}
+              className="bg-transparent border-0"
+            >
+              <i className="fa solid fa-pause" style={{ color: "white" }}></i>
+            </button>
           ) : (
-            <button onClick={() => } className="bg-transparent border-0">
+            <button
+              onClick={() => {
+                setIsPlaying(true);
+              }}
+              className="bg-transparent border-0"
+            >
               <i className="fa solid fa-play" style={{ color: "white" }}></i>
             </button>
           )}
-          <button className="skipButtons text-center border rounded border-0">
+          <button
+            className="skipButtons text-center border rounded border-0"
+            onClick={() => handleRight()}
+          >
             <i className="fa solid fa-caret-right"></i>
           </button>
         </div>
